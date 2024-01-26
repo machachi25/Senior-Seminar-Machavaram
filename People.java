9fcbd2dbe61ce39d483df9c2ad38de738e21a099
Chinmay Machavaram <machachi25@7hills.org>
2024-01-25 20:53:38 -0500

    Added a bunch of txt files, adn then I can call them individually

--- People.java
+++ People.java
@@ -1,19 +1,32 @@
-public class People {
-   private String name;
-   private String email;
-   
-   public People(String nameOne, String emailOne)
-   {
-      name = nameOne;
-      email = emailOne;
-   }
-   public void Greet()
-   {
-      System.out.pritnln("Hello!");
-   }
-   
-   class Student extends People()
-   {}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
 
-    
+public class People 
+{
+   private String name;
+   ArrayList<Senior> seniors = new ArrayList<Senior>();
+   public People(String initName)
+   {
+      name = initName;
+   }
+
+   public void presentersFile()
+   {
+      try {
+      File myObj = new File("Presenters.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        seniors.add(Senior data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+   }
 }
+    
+
--- Senior.java
+++ Senior.java
@@ -1,4 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Senior
 {
-    
+    private String nombre;
+
+    public Senior(String initNombre)
+    {
+        nombre = initNombre;
+    }
+
+    public void Graduates()
+    {
+        try {
+      File myObj = new File("filename.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String info = myReader.nextLine();
+        seniors.add(info);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    }
 }
+
